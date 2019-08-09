4a50700171531d4f7a1ac20422ccf935f2c014a0
其妙 <hling51325@163.com>
2019-08-09 17:37:04 +0800

    update

--- example/mongoShell.js
+++ example/mongoShell.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose')
 const ObjectId = mongoose.Types.ObjectId
 
-// util mongo -u "mongodb://root:root@127.0.0.1:27017/admin?authSource=admin" -s "./example/test.js"
 module.exports = async (collections) => {
-  console.log(Object.keys(collections))
   const user = await collections['system.users'].findOne({})
   console.log(user)
 }
---
+++ example/shell.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+const CWD = `${process.cwd()}`
+
+const script = process.argv[2]
+connect()
+    .then(async () => {
+        let colls = await mongoose.connection.db.listCollections().toArray()
+        const collMap = await colls.reduce((map, next) => {
+            map[next.name] = mongoose.connection.db.collection(next.name)
+            return map
+        }, {})
+        await require(`${CWD}/${script}`)(collMap, mongoose.connection.db)
+    })
+    .catch(e => {
+        console.error(e)
+        process.exit(1)
+    })
+    .finally(async () => {
+        await mongoose.disconnect()
+        process.exit(0)
+    })
+
+async function connect() {
+    try {
+        await mongoose.connect(url, {
+            useNewUrlParser: true
+        })
+    } catch (e) {
+        console.error(e)
+    }
+}
