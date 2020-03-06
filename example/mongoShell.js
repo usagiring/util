fc2995dc1920117c15afe9b82c973fc9a94b043f
其妙 <hling51325@163.com>
2020-03-06 11:12:36 +0800

    update example

--- example/mongoShell.js
+++ example/mongoShell.js
@@ -4,4 +4,5 @@ const ObjectId = mongoose.Types.ObjectId
 module.exports = async (collections) => {
   const user = await collections['system.users'].findOne({})
   console.log(user)
+  // collections['user'].createIndex({ _id: 1 }, { background: true })
 }
