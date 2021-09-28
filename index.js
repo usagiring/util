1df79693a1109cf24f81b83445700490b983710e
其妙 <hling51325@163.com>
2021-09-28 14:21:20 +0800

    fix

--- index.js
+++ index.js
@@ -3,34 +3,34 @@
 const program = require('commander')
 const { url, json, mongo, uuid, base64 } = require('./lib')
 
-program.version('0.0.1')
+program.version('0.0.2')
 
 program
-    .command('url')
-    .option('-d, --decode <params>')
-    .option('-e, --encode <params>')
-    .action(url)
+  .command('url')
+  .option('-d, --decode <params>')
+  .option('-e, --encode <params>')
+  .action(url)
 
 program
-    .command('json')
-    .option('-p, --parse <params>')
-    .option('-s, --stringify <params>')
-    .action(json)
+  .command('json')
+  .option('-p, --parse <params>')
+  .option('-s, --stringify <params>')
+  .action(json)
 
 program
-    .command('mongo')
-    .option('-u, --url <params>')
-    .option('-s, --script <params>')
-    .action(mongo)
+  .command('mongo')
+  .option('-u, --url <params>')
+  .option('-s, --script <params>')
+  .action(mongo)
 
 program
-    .command('uuid')
-    .action(uuid) 
+  .command('uuid')
+  .action(uuid)
 
 program
-    .command('base64')
-    .option('-d --decode <params>')
-    .option('-e --encode <params>')
-    .action(base64)  
+  .command('base64')
+  .option('-d --decode <params>')
+  .option('-e --encode <params>')
+  .action(base64)
 
 program.parse(process.argv);
