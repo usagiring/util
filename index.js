714d89186496af803b73fbd99433ad1f6d42b33f
其妙 <hling51325@163.com>
2022-03-21 16:00:34 +0800

    update

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
 const program = require('commander')
-const { url, json, mongo, uuid, base64 } = require('./lib')
+const { url, json, mongo, uuid, base64, objectid2date } = require('./lib')
 
-program.version('0.0.2')
+program.version('0.0.3')
 
 program
   .command('url')
@@ -32,5 +32,10 @@ program
   .option('-d --decode <params>')
   .option('-e --encode <params>')
   .action(base64)
+
+program
+  .command('objectid2date')
+  .argument('<string>')
+  .action(objectid2date)
 
 program.parse(process.argv);
